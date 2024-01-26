b8d8e983534be459007f931b000f000026722197
Edkar Chachati <chachati28@gmail.com>
2024-01-26 16:05:05 -0400

    loop

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ fn main() {
     let name: &str = "John";
     println!("I am {} and I am {} years old", name, age);
 
-    get_data_from_user()
+    get_data_from_user();
+
+    loop_example();
 }
 
 fn get_data_from_user() {
@@ -47,4 +49,32 @@ fn conditional_example(age: u8) -> &'static str {
     } else {
         return "You are a child";
     }
+}
+
+fn loop_example() {
+    println!("Enter number 1: ");
+    let number_1 = get_number_from_user();
+    println!("Enter number 2: ");
+    let number_2 = get_number_from_user();
+
+    loop {
+        let sum: u16 = (number_1 + number_2).into();
+
+        println!("What is the sum of {} and {}? ", number_1, number_2);
+        let user_sum: u16 = get_number_from_user();
+
+        if user_sum == sum {
+            println!("You are correct");
+            break;
+        } else {
+            println!("You are wrong. Try again");
+        }
+    }
+}
+
+fn get_number_from_user() -> u16 {
+    let mut number = String::new();
+    std::io::stdin().read_line(&mut number).unwrap();
+    let number_as_number: u16 = number.trim().parse().unwrap();
+    return number_as_number;
 }
